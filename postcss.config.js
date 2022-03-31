998cc03664e440accd16f31c1cb6138477f9ed46
Khalsa-jio <rv.singh462@gmail.com>
2022-03-31 14:14:56 +0530

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -5,7 +5,7 @@ module.exports = {
       ? [
           "@fullhuman/postcss-purgecss",
           {
-            content: ["./pages/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+            content: ["./pages/**/*.tsx", "./components/**/*.tsx"],
             defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
           },
         ]
